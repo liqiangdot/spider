26c77431603eac2531663e44935380257684e281
LiQiang <LiQiang@HuaFoun.Com>
2021-02-24 20:12:34 +0800

    继续优化

--- main.py
+++ main.py
@@ -1,13 +1,124 @@
+# coding=utf-8
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import random
+import time
+from ebooklib import epub
 from zhconv import convert
 from requests_html import HTMLSession
 from requests_html import HTML
 
 root_url = "https://mickey1124.pixnet.net/"
+absolute_dict = {}
+absolute_site = {}
+
+def create_pub():
+    book = epub.EpubBook()
+
+    # add metadata
+    book.set_identifier('')
+    book.set_title('基督教小小羊園地')
+    book.set_language('cn')
+
+    book.add_author('小小羊')
+
+    # intro chapter
+    c1 = epub.EpubHtml(title='简介', file_name='intro.xhtml', lang='cn')
+    c1.content = u'<html><head></head><body><h1>約翰福音8：32</h1><p>你們必曉得真理，真理必叫你們得以自由</p></body></html>'
+
+    # defube style
+    style = '''BODY { text-align: justify;}'''
+
+    default_css = epub.EpubItem(uid="style_default", file_name="style/default.css", media_type="text/css",
+                                content=style)
+    book.add_item(default_css)
+
+    # about chapter
+    c2 = epub.EpubHtml(title='About this book', file_name='about.xhtml')
+    c2.content = '<h1>About this book</h1><p>Helou, this is my book! There are many books, but this one is mine.</p>'
+    c2.set_language('cn')
+    c2.properties.append('rendition:layout-pre-paginated rendition:orientation-landscape rendition:spread-none')
+    c2.add_item(default_css)
+
+    # add chapters to the book
+    book.add_item(c1)
+    book.add_item(c2)
+
+    # create table of contents
+    # - add manual link
+    # - add section
+    # - add auto created links to chapters
+
+    book.toc = (epub.Link('intro.xhtml', '简介', 'intro'),
+                (epub.Section('Languages'),
+                 (c1, c2))
+                )
+
+    # add navigation files
+    book.add_item(epub.EpubNcx())
+    book.add_item(epub.EpubNav())
+
+    # define css style
+    style = '''
+@namespace epub "http://www.idpf.org/2007/ops";
+body {
+    font-family: Cambria, Liberation Serif, Bitstream Vera Serif, Georgia, Times, Times New Roman, serif;
+}
+h2 {
+     text-align: left;
+     text-transform: uppercase;
+     font-weight: 200;     
+}
+ol {
+        list-style-type: none;
+}
+ol > li:first-child {
+        margin-top: 0.3em;
+}
+nav[epub|type~='toc'] > ol > li > ol  {
+    list-style-type:square;
+}
+nav[epub|type~='toc'] > ol > li > ol > li {
+        margin-top: 0.3em;
+}
+'''
+
+    # add css file
+    nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
+    book.add_item(nav_css)
+
+    # create spine
+    book.spine = ['nav', c1, c2]
+
+    # create epub file
+    epub.write_epub('test.epub', book, {})
+
+    return
+
+def get_host_content():
+    host_str = ""
+    file_str = ""
+    get_indexs()
+    i = 0
+    for category in absolute_dict:
+        # index -> category
+        # absolute_dict[index] -> link
+        file_str = "\n@@@@@@ " + category + "@@@@@@\n"
+        for link in absolute_dict[category]:
+            time.sleep(random.randint(0,5))
+            i += 1
+            print("即将下载链接[" + str(i) + "]：" + link)
+            page_dict = get_page(link)
+            for title in page_dict:
+                file_str += "\n&&&&&& " + title + " &&&&&&\n"
+                file_str += "\n" + page_dict[title] + ""
+            write_file(file_str)
+
+    #print(file_str)
+    return
 
 def get_common_content(common_str):
     html = HTML(html=common_str)
@@ -20,7 +131,7 @@ def get_common_content(common_str):
         common_time = str(item.xpath(xpath_str, first=True).text)
         xpath_str = "//*[contains(@class,'user-name')]"
         common_author = str(item.xpath(xpath_str, first=True).text)
-        common_info = "作者：\n" + common_author + "\n时间：" + common_time + "\n" + common_text
+        common_info = "作者：" + common_author + "\n时间：" + common_time + "\n" + common_text
         common_list.append(common_info)
         #print("时间：\n" + common_time)
         #print("作者：\n" + common_author)
@@ -28,40 +139,52 @@ def get_common_content(common_str):
 
     return common_list
 
+def write_file(s):
+    fo = open("1.txt", "a", encoding="utf-8")
+    fo.write(s)
+    fo.close()
+    return
+
 def get_indexs():
     proxies = {"https": "http://127.0.0.1:1082"}
     session = HTMLSession()
     r = session.get('https://mickey1124.pixnet.net/blog', proxies=proxies)
     text_link_dict = {}
 
-    for i in range(1, 23):
+    for i in range(1, 24): # 1 ~23
         xpath_str = "//*[@id=\"category\"]/div/ul/li[" + str(i) + "]"
-        text = (r.html.xpath(xpath_str, first=True).text)
-        text_link = (r.html.xpath(xpath_str, first=True).absolute_links)
-        text_link_dict[text] = text_link
-    return text_link_dict
+        text_title = (r.html.xpath(xpath_str, first=True).text)
+        print("分类名称：" + text_title)
+        text_links = (r.html.xpath(xpath_str, first=True).absolute_links)
+        tmp_list = []
+        for link in text_links:
+            print("当前序号：" + str(i) + " - 23")
+            i = i + 1
+            print("分类链接：" + link)
+            page_number = get_catagory_pages(link)
+            print("分类页码：" + str(page_number))
+            for i in range(1, page_number + 1):
+                url = link + "/" + str(i)
+                ret_list = get_category_links(url)
+                tmp_list.extend(ret_list)
+
+        absolute_dict[text_title] = tmp_list
+
+    return absolute_dict
 
 def get_last_number(url):
     end = "/"
     return url[url.rfind(end) + 1:]
 
 def get_catagory_pages(url):
-    return
-
-def get_category_info(url):
     proxies = {"https": "http://127.0.0.1:1082"}
     post_link = "https://mickey1124.pixnet.net/blog/post/"
-    ret_list = []
-    link_list = []
+
     page_list = []
     self_number = get_last_number(url)
 
     session = HTMLSession()
     r = session.get(url, proxies=proxies)
-
-    xpath_str = "//*[@id=\"article-box\"]/h3"
-    category_text = str(r.html.xpath(xpath_str, first=True).text)
-    print(category_text)
 
     for link in r.html.absolute_links:
         ret = link.find("comments")
@@ -74,21 +197,16 @@ def get_category_info(url):
             if page != self_number:
                 page_list.append(int(page))
 
-    page_total = max(page_list)
-    print(page_total)
-
-    ret_list[0] = url
-    ret_list[1] = category_text
-    ret_list[2] = page_total
-
-    return ret_list
+    if len(page_list):
+        page_total = max(page_list)
+    else:
+        page_total = 1
+    return page_total
 
 def get_category_links(url):
     proxies = {"https": "http://127.0.0.1:1082"}
     post_link = "https://mickey1124.pixnet.net/blog/post/"
     link_list = []
-    page_list = []
-    self_number = get_last_number(url)
 
     session = HTMLSession()
     r = session.get(url, proxies=proxies)
@@ -110,33 +228,42 @@ def get_common(url):
     session = HTMLSession()
     r = session.get(common_url, proxies=proxies)
     r_dict = r.json()
+
     common_list = get_common_content(r_dict['list'])
     return common_list
 
 def get_page(url):
     proxies = {"https": "http://127.0.0.1:1082"}
+    ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0'
     session = HTMLSession()
-    r = session.get(url, proxies=proxies)
-    #r.html.render()
-    #links = r.html.absolute_links
-    #for link in links:
-    #    print(link)
-    #print(r.html.text)
+    r = session.get(url, proxies=proxies, headers={'user-agent': ua})
+    #r.html.render(scrolldown=5,sleep=3)
+    page_dict = {}
+    self_number = get_last_number(url)
     xpath_str = "//*[@id=\"article-box\"]/div/ul/li[1]"
     page_time = r.html.xpath(xpath_str, first=True).text
-    print("时间：" + page_time)
+    #print("时间：" + page_time)
 
-    xpath_str = "//*[@id=\"article-329364895\"]"
+    xpath_str = "//*[@id=\"article-" + self_number + "\"]"
     title = r.html.xpath(xpath_str, first=True).text
-    print("标题：" + title)
+    #print("标题：" + title)
+    title = convert(title, 'zh-hans')
+
     xpath_str = "//*[@id=\"article-content-inner\"]"
     content = r.html.xpath(xpath_str, first=True).text
 
-    content = convert(content, 'zh-hans')
-    print("内容：" + content)
+    format_text = "时间：" + page_time + '\n' + content
+
     common_list = get_common(url)
-    print(common_list)
-    return
+    format_text += "\n****** 注释 ******\n"
+    for common in common_list:
+        format_text += common
+
+    #format_text = convert(format_text, 'zh-hans')
+    page_dict[title] = format_text
+    print(page_dict)
+    #write_file(format_text)
+    return page_dict
 
 def get_page_text():
     text = ""
@@ -145,31 +272,15 @@ def get_page_bible():
     bible = ""
     return bible
 
-#page = get_page('https://mickey1124.pixnet.net/blog/post/329364895')
-get_category_links('https://mickey1124.pixnet.net/blog/category/3270852')
-
-#print(page)
+#write_file("sss")
+#create_pub()
+#get_indexs()
+#get_host_content()
+page = get_page('https://mickey1124.pixnet.net/blog/post/269195376')
+#get_category_links('https://mickey1124.pixnet.net/blog/category/3270852')
 
 #with open("a.txt",'w',encoding='utf-8') as f:
 #    f.write(response.text)
 
-
-#import requests
-#import random
-
-#proxies = [
-#    {'http':'socks5://127.0.0.1:1081'},
-#    {'https':'socks5://127.0.0.1:1081'}
-#]
-#proxies = random.choice(proxies)
-#print(proxies)
-#url = 'https://mickey1124.pixnet.net/blog'
-#try:
-#    response = requests.get(url,proxies=proxies) #使用代理
-#    print(response.status_code)
-#    if response.status_code == 200:
-#        print(response.text)
-#except requests.ConnectionError as e:
-#    print(e.args)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
